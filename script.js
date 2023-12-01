5dac408caf46976b500beaf502fb53eb64e495b9
MaterialesSecundaria <79664447+MaterialesSecundaria@users.noreply.github.com>
2023-12-01 16:44:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,573 @@
+  /* CARGA PPT 2024*/
+  const featuresList = [
+    {
+      icon: "https://res.cloudinary.com/dr8dnxnca/image/upload/v1689395120/blogger_1/progress-icon_lxhego.svg",
+      title: "Álgebra ",
+      description:
+        "PPT 2024.",
+      link: "https://drive.google.com/drive/folders/1xfU9Dg9ZJEvMN26tYDIxSOCjnVyBaVMe?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_arit.png",
+      title: "Aritmética",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1DHIESsAWonublEogupU0TiJMfIiBrHuw?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_geoms.png",
+      title: "Geometría",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1B_ouMrlpnXw2IGL2AsCVrFlUQ7ohSZwT?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_trigs.png",
+      title: "Trigonometría",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1v7vHNVyrgsitQ9XyjDllnoJzd5oKtNva?usp=sharing",  
+    },
+  
+    {
+      icon: "assets/icono_comp.png",
+      title: "Raz. Matemático",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1UMj4mWR7SBvp2k2ALEZFDeW4yQvba0_V?usp=sharing",   
+    },
+  
+    {
+      icon: "assets/icono_fis.png",
+      title: "Física",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1eZCgnB4pvFpOdcFZjvgamekTlNXYKXm3?usp=sharing",
+    },
+     {
+      icon: "assets/icono_quim.png",
+      title: "Química",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1uZd9v3JsJF_8xIGlV-E6jceAi4brmthz?usp=sharing",
+    },
+     {
+      icon: "https://res.cloudinary.com/dr8dnxnca/image/upload/v1689395117/blogger_1/create-task-icon_bjbqxc.svg",
+      title: "Lenguaje",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/14Ej7k8GPvptXB6_-2fz7G9EzBkBsp3Jt?usp=sharing",
+    },
+      {
+      icon: "assets/icono_rv.png",
+      title: "Raz Verbal",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/icono_comps.png",
+      title: "Computación",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+  
+      
+  ];
+    /* CARGA BOLETIN 2024*/
+  const featuresListDos = [
+    {
+      icon: "assets/icono_par.png",
+      title: "Álgebra ",
+      description:
+        "Boletin 2023.",
+      link: "https://drive.google.com/drive/folders/1TiKAXy_tXN8E4IkBdYvK03tVVSozMfZD?usp=sharing",
+    },  
+    {
+      icon: "assets/icono_arit.png",
+      title: "Aritmética",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/13g-ek24cdMYTvYrl5S_Ty83rdlFthJAX?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_geoms.png",
+      title: "Geometría",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/1SFSylq01FIT3KObmg4JdoEFooZ5XWcJ6?usp=sharing",  
+    },
+  
+    {
+      icon: "assets/icono_trigs.png",
+      title: "Trigonometría",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/1vBX27f2cNUtk2wfuKHxqYXvKfPndALUi?usp=sharing",      
+    },
+  
+    {
+      icon: "assets/icono_comp.png",
+      title: "Raz. Matemático",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/1hAKFQRuXr1i_-AjlW5xu5lO0SuF3V9kG?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_fis.png",
+      title: "Física",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/1VdOFiSFPmG-aGQx5gKsxLWsy_Zt39zJB?usp=sharing",
+    },
+     {
+      icon: "assets/icono_quim.png",
+      title: "Química",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/1_4-g9gxOOg389ilG3q9nedQ4Nss9ht8Z?usp=sharing",
+    },
+     {
+      icon: "assets/icono_leng.png",
+      title: "Lenguaje",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/1B55DOLK14qQUVQ2qLi7Es3MwbjhK-TyE?usp=sharing",
+    },
+      {
+      icon: "assets/icono_rv.png",
+      title: "Raz. Verbal",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/10Sv7ahhF6ddSS9VLQxB3pmOIe1LrDGyT?usp=sharing",  
+    },
+    {
+      icon: "assets/icono_comps.png",
+      title: "Computación",
+      description:
+        "Boletin 2024.",
+      link: "https://drive.google.com/drive/folders/1u3YBFFSsYCMYw15psd25DWOQJAn-PtI0?usp=sharing",  
+    },
+    
+  ];  
+  /* CRONORAMA*/
+  const specificationsData = [
+    {
+      category: "Boletines Filter 1",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Octubre 23, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Octubre 23, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Octubre 23, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 06, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Noviembre 06, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Noviembre 06, 2023 (filtro #1)",
+        },
+        
+      ],
+    },
+  
+    {
+      category: "Boletines Filter 2",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 06, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Noviembre 06, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Noviembre 06, 2023 (filtro #2)",
+        },
+      ],
+    },
+    {
+      category: "PPT'S actualizaciones",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Noviembre 03, 2023",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Noviembre 10, 2023",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Noviembre 17, 2023",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 24, 2023",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Diciembre 01, 2023",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Diciembre 08, 2023",
+        },
+      ],
+    },
+  ];
+  /* RESPONSABLE */
+  const specificationsDataTwo = [
+    {
+      category: "Boletines Filter 1",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Octubre 23, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Octubre 23, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Octubre 23, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 11, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Noviembre 11, 2023 (filtro #1)",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Noviembre 11, 2023 (filtro #1)",
+        },
+        
+      ],
+    },
+  
+    {
+      category: "Boletines Filter 2",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 11, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Noviembre 11, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Noviembre 11, 2023 (filtro #2)",
+        },
+      ],
+    },
+    {
+      category: "PPT'S actualizaciones",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Noviembre 03, 2023",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Noviembre 10, 2023",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Noviembre 17, 2023",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 24, 2023",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Diciembre 01, 2023",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Diciembre 08, 2023",
+        },
+      ],
+    },
+  ];
+
+  const specificationsContainer = document.querySelector(".specifications-container");
+  const specificationsContainerTwo = document.querySelector(".specifications-container-two");
+  const featuresContent = document.querySelector("#features .content");  
+  const featuresContentDos = document.querySelector("#ppt23 .content");  
+    
+  const menuIcon = document.querySelector(".menu-icon");
+  const mobileNavMenu = document.querySelector(".mobile-nav-menu");
+  const navLinks = document.querySelectorAll(".nav-link");
+  let currentTestimonialIndex = 0;  
+    
+  /* ppt 2024*/  
+  const displayFeatures = () => {
+    featuresList.forEach((f) => {
+      const html = `<div class="icon">
+          <img src="${f.icon}" alt="" />
+        </div>
+        <h3>${f.title}</h3>
+        <p>
+          ${f.description}
+        </p>
+         <a href="${f.link}" target='_blank' class="btn-dos">Ingrese</a>`;
+  
+      const featureCard = document.createElement("div");
+      featureCard.classList.add("feature-card");
+      featureCard.innerHTML = html;
+  
+      featuresContent.appendChild(featureCard);
+    });
+  };
+  
+  displayFeatures();
+  /*End  ppt 2024*/  
+    
+  /* boletin 2024*/
+    
+    const displayFeaturesDos = () => {
+    featuresListDos.forEach((f) => {
+      const htmlune = `<div class="icon">
+          <img src="${f.icon}" alt="" />
+        </div>
+        <h3>${f.title}</h3>
+        <p>
+          ${f.description}
+        </p>
+         <a href="${f.link}" target='_blank' class="btn">Ingrese</a>`;
+  
+      const featureCardDos = document.createElement("div");
+      featureCardDos.classList.add("feature-card");
+      featureCardDos.innerHTML = htmlune;
+  
+      featuresContentDos.appendChild(featureCardDos);
+    });
+  };
+  
+  displayFeaturesDos();
+  
+  /* End boletin 2024 */
+    
+  const accordions = document.querySelectorAll('.accordion');
+  
+    for(accordion of accordions) {
+  
+       accordion.onclick = function () {
+          this.classList.toggle('active');
+  
+          this.firstElementChild.classList.toggle('active');
+          let panel = this.lastElementChild;
+  
+          if (panel.style.height) {
+             panel.style.height = null; 
+  
+          } else{
+             panel.style.height = panel.scrollHeight + 'px'; 
+          }
+      }
+  };  
+    
+  
+  menuIcon.addEventListener("click", () => {
+    mobileNavMenu.classList.toggle("active");
+  });
+  
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        const offset = targetElement.offsetTop - 60;
+        window.scrollTo({ top: offset });
+      }
+  
+      mobileNavMenu.classList.remove("active");
+    });
+  });  
+
+
+  const generateHTML = () => {
+    specificationsData.forEach((s) => {
+      const tableRowsHTML = s.details
+        .map((detail) => {
+          return `<tr>
+          <th>${detail.title}</th>
+          <td>${detail.value}</td>
+        </tr>`;
+        })
+        .join("");
+  
+      const HTML = `<h3>
+          ${s.category} (${s.details.length})
+          <span class="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </span>
+        </h3>
+        <table>
+            <tbody>
+              ${tableRowsHTML}
+            </tbody>
+          </table>`;
+  
+      const category = document.createElement("div");
+      category.classList.add("category");
+      category.innerHTML = HTML;
+  
+      specificationsContainer.appendChild(category);
+    });
+  };
+  
+  generateHTML();
+  
+  const categoryHeadings = document.querySelectorAll(
+    ".specifications-container h3"
+  );
+  
+  categoryHeadings.forEach((h) => {
+    h.addEventListener("click", () => {
+      h.classList.toggle("active");
+      const table = h.nextElementSibling;
+  
+      if (h.classList.contains("active")) {
+        table.style.display = "table";
+        return;
+      }
+  
+      table.style.display = "none";
+    });
+  });
+
+  /* RESPONSABLE */
+
+  const generateTwoHTML = () => {
+    specificationsDataTwo.forEach((p) => {
+      const tableRowsTwoHTML = p.details
+        .map((detail) => {
+          return `<tr>
+          <th>${detail.title}</th>
+          <td>${detail.value}</td>
+        </tr>`;
+        })
+        .join("");
+  
+      const HTML = `<h3>
+          ${p.category} (${p.details.length})
+          <span class="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </span>
+        </h3>
+        <table>
+            <tbody>
+              ${tableRowsTwoHTML}
+            </tbody>
+          </table>`;
+  
+      const categoryTwo = document.createElement("div");
+      categoryTwo.classList.add("category");
+      categoryTwo.innerHTML = HTML;
+  
+      specificationsContainerTwo.appendChild(category);
+    });
+  };
+  
+  generateTwoHTML();
+  
+  const categoryHeadingsTwo = document.querySelectorAll(
+    ".specifications-container-two h3"
+  );
+  
+  categoryHeadingsTwo.forEach((q) => {
+    q.addEventListener("click", () => {
+      q.classList.toggle("active");
+      const tableTwo = q.nextElementSibling;
+  
+      if (q.classList.contains("active")) {
+        tableTwo.style.display = "table";
+        return;
+      }
+  
+      tableTwo.style.display = "none";
+    });
+  });
+
+  /* END-RESPONSABLE */
+    
